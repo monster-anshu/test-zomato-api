1421ef64f2b3ff84569b121861a974a7beeb4b97
monster anshu <hgunwant2312@gmail.com>
2022-03-26 14:38:00 +0530

    Commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const DB = require("./Database/db");
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello Api is working");
