571a4788f8ce87312f22a531d142b4cc545c08ed
monster anshu <hgunwant2312@gmail.com>
2022-03-27 10:04:51 +0530

    Commit

--- index.js
+++ index.js
@@ -5,7 +5,23 @@ const DB = require("./Database/db");
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.redirect("https://app.swaggerhub.com/apis-docs/monster-anshu/Zomato_Api/1.0.0")
+  res.send(`
+  <h1 style='text-align:center' >
+  Base Url of Api : http://zomatoapu.herokuapp.com
+  </h1>
+  <h1>
+    <a target="_blank" href="https://github.com/monster-anshu/Zomato.git">View Source Code</a>
+  </h1>
+  <h1>
+    <a target="_blank" href="https://app.swaggerhub.com/apis-docs/monster-anshu/Zomato_Api/1.0.0">View Documentation</a>
+  </h1>
+  <h1>
+    <a target="_blank" href="https://www.monster-anshu.cf">View my Portfolio</a>
+  </h1>
+  <h1>
+    <a target="_blank" href="https://github.com/monster-anshu">View my Github</a>
+  </h1>
+  `);
 });
 app.use("/cat", require("./routes/catogories"));
 app.use("/items", require("./routes/items"));
